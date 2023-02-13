62d07499dce4412d3ccc0e5e8c08c86e04239620
LuanYi0211 <84923732+LuanYi0211@users.noreply.github.com>
2023-02-13 19:24:25 +0800

    Update Writings.vue

--- src/views/Writings.vue
+++ src/views/Writings.vue
@@ -1,10 +1,67 @@
 <template>
-    <h1>著作</h1>
+    <div>
+        <h1>著作</h1>
+        <hr>
+        <el-row>
+            <el-col :span="7" v-for="item in patents" :key="item.title" :index="item.title">
+                <el-card class="box-card">
+                    <div slot="header" class="clearfix"><span>{{ item.title }}</span></div>
+                    <el-image 
+                        style="width: 200px; height: 300px"
+                        :src="item.img" 
+                        :preview-src-list="item.imgBig">
+                    </el-image>
+                    <!-- <img :src="item.img"> -->
+                </el-card>
+            </el-col>
+        </el-row>
+    </div>
 </template>
 <script>
 export default {
     data() {
-        return {}
+        return {
+            patents: [
+                {
+                    title: '应用文自动生成方法',
+                    img: require('../assets/166300455563330069324568.jpg'),
+                    imgBig: [require('../assets/166300455563330069324568.jpg'),
+                    require('../assets/1501277314297.jpg')]
+                },
+                {
+                    title: '应用文自动生成方法',
+                    img: require('../assets/166300455563330069324568.jpg'),
+                    imgBig: [require('../assets/166300455563330069324568.jpg')]
+                },
+                {
+                    title: '应用文自动生成方法',
+                    img: require('../assets/166300455563330069324568.jpg'),
+                    imgBig: [require('../assets/166300455563330069324568.jpg')]
+                },
+                {
+                    title: '应用文自动生成方法',
+                    img: require('../assets/166300455563330069324568.jpg'),
+                    imgBig: [require('../assets/166300455563330069324568.jpg')]
+                },
+                {
+                    title: '应用文自动生成方法',
+                    img: require('../assets/166300455563330069324568.jpg'),
+                    imgBig: [require('../assets/166300455563330069324568.jpg')]
+                },
+            ]
+        }
     }
 }
 </script>
+<style>
+/* .box-card {
+    width: 300px;
+} */
+.el-card {
+    margin-right: 50px;
+}
+.el-col {
+    margin-bottom: 20px;
+    margin-right: 50px;
+}
+</style>
